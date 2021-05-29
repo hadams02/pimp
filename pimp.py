1c4579739089f0a8dda699f9f57891f799018643
hadams02 <84363552+hadams02@users.noreply.github.com>
2021-05-29 02:10:08 -0500

    Update pimp.py
    removed nonfunctional music commands, will work on finding working solution

--- pimp.py
+++ pimp.py
@@ -5,6 +5,7 @@ import discord
 import random
 import requests
 import youtube_dl
+import asyncio
 from discord import FFmpegPCMAudio
 from bs4 import  BeautifulSoup
 from discord.ext import commands
@@ -30,7 +31,7 @@ async def on_member_join(member):
 @bot.event
 async def on_member_remove(member):
     channel = client.get_channel(820511759155855362)
-    embed=discord.Embed(title=f"Leave and take ya homeboys with you {member.name}", description=f"And dont ask for no handouts from {member.guild.name}!") 
+    embed=discord.Embed(title=f"Leave and take ya homeboys with ya {member.name}", description=f"And dont ask for no handouts from {member.guild.name}!") 
     embed.set_thumbnail(url=member.avatar_url) 
 
     await channel.send(embed=embed)
@@ -95,7 +96,16 @@ async def crypto(ctx, arg):
         em = discord.Embed(title=getText, colour=0xba9f34)
         em.set_author(name='Dogecoin', icon_url='https://static.wikia.nocookie.net/dogecoin/images/c/c9/Logo.png/revision/latest?cb=20180917222934')
         await ctx.send('', embed=em)
-    
+    elif (arg=='ltc'):
+        r = s.get("https://www.coindesk.com/price/litecoin")
+        soup = BeautifulSoup(r.content, 'html.parser')
+        price = soup.find('div', class_ ="price-large")
+        getText = price.getText()
+        em = discord.Embed(title=getText, colour=0xba9f34)
+        em.set_author(name='Litecoin', icon_url='https://upload.wikimedia.org/wikipedia/commons/f/f8/LTC-400.png')
+        await ctx.send('', embed=em)
+    elif (arg=='list'):
+        await ctx.send('')
     
 ## Coinflip command
 heads = "Heads"
@@ -104,80 +114,5 @@ tails = "Tails"
 @bot.command()
 async def coinflip(ctx):
     await ctx.send(random.choice([heads, tails]))
-
-
-
-## Youtube DL
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-
-## Leave and join vc
-@bot.command()
-async def join(ctx):
-    if not ctx.message.author.voice:
-        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
-        return
-    else:
-        channel = ctx.message.author.voice.channel
-    await channel.connect()
-
-@bot.command()
-async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    if voice_client.is_connected():
-        await voice_client.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-## Music commands
-@bot.command()
-async def play(ctx,url):
-    try :
-        server = ctx.message.guild
-        voice_channel = server.voice_client
-
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
-        await ctx.send('**Now playing:** {}'.format(filename))
-    except:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-
-
 
 bot.run('ODQzNjg5ODY0ODk2ODM5Njkx.YKHhag.GGZlzHfOfSJI523u0x7HthurNKE')
