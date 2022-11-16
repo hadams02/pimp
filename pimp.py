4eec659ff26faa3e150fd7b4e6f1123ab2737101
hadams02 <84363552+hadams02@users.noreply.github.com>
2022-11-16 08:22:16 -0600

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -5,6 +5,7 @@ import discord
 import random
 import requests
 import youtube_dl
+import os
 from discord import FFmpegPCMAudio
 from bs4 import  BeautifulSoup
 from discord.ext import commands
@@ -45,6 +46,8 @@ async def passtheblunt(channel):
     channel = client.get_channel(820511759155855362)
     await channel.send("help yaself g", file=discord.File('/Users/hunter/Desktop/pimp/pass.png'))
     
+
+## discord.Sticker
 
 ##Random biggie lyric command
 my_list = ["i love it when they call me big poppa", "throw ya hands in the air if u a true playa", "super nintendo sega genesis when i was dead broke man i couldnt picture this"]
@@ -103,7 +106,67 @@ async def crypto(ctx, arg):
         await ctx.send('', embed=em)
     elif (arg=='list'):
         await ctx.send('')
+
+## Music commands
+@bot.command()
+async def play (ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current song to be over or use the !stop command")
+        return
+
+    channel.VoiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Lobby')
+    await channel.VoiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3")) 
+
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await channel.VoiceChannel.disconnect()
+    else:
+        await ctx.send("pimp is not connected to a voice channel.")
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        ctx.send("No audio is being played.")
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        ctx.send("The audio is not paused")
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+
 ## Coinflip command
 heads = "Heads"
 tails = "Tails"
